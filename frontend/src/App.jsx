fc1344ac3890a9ef6efd40b15c1d7a18f87c22fa
AngelSalazarV <angel152265@gmail.com>
2025-10-15 00:30:19 -0500

    Project initialization, complete configuration and installation of TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex w-full h-screen justify-center items-center'>
+        <h1 className='text-5xl font-bold'>WOWIPLAT</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
